a3d18cc2522e23cc20bcc2b68877ae540b2978b6
Lenovo <sonp2401@gmail.com>
2020-09-01 00:09:02 +0300

    commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,30 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.view.View.OnClickListener;
 import android.widget.TextView;
-
-import java.text.BreakIterator;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-  public Button button;
+    public Button button;
+    static int count = 0;
+    public TextView textView;
 
     @Override
-  public   void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         button=(Button) findViewById(R.id.button);
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
         button.setOnClickListener(this);
     }
 
- static int count=0;
     @Override
     public void onClick(View v) {
-       count++;
-        TextView textView=(TextView) findViewById(R.id.textView);
-        textView.setText(count+"");
-        button.setText(count+"");
-
+        count++;
+        textView.setText(count + "");
     }
 }
